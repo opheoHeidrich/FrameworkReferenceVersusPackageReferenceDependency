9f6e096caef1d96243c32842690cbcce5a0b0b82
René Heidrich <rene.heidrich@opheo.com>
2023-05-10 13:02:31 +0200

    Changed the readme to contain a how to

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # FrameworkReference versus PackageReference-dependency
 This repository shows, that NuGet packages, with dependencies to packages of framework DLLs, will copy them to the output. This is inconsistent with direct references to the framework DLLs.
 
-In the logs the task 'PackageReferenceDependency' seems to be unable to properly resolve the conflicts between the NuGet- and Framework-References. For example the reference to 'Microsoft.AspNetCore.Http.Connections.Common'. The output for the 'Reference' task ends up with the version 6.0.16 from the framework, but the ReferenceCopyLocalPaths contains Version 6.0.13 from NuGet. At the end of the build the .deps.json and DLL in the output folder are both 6.0.13 from NuGet.
+# How to reproduce the bug:
+Run the '**ReproduceBug.ps1**' and compare the outputs for the sample projects.
+
+# Unexpected behavior
+In the logs the task 'PackageReferenceDependency' seems to be unable to properly resolve the conflicts between the NuGet- and Framework-References. For example the reference to 'Microsoft.AspNetCore.Http.Connections.Common'. The output for the 'Reference' task ends up with the version 6.0.16 from the framework, but the ReferenceCopyLocalPaths contains Version 6.0.13 from NuGet. At the end of the build the *.deps.json and DLL in the output folder are both 6.0.13 from NuGet.
--- ReproduceBug.ps1
+++ ReproduceBug.ps1
@@ -5,7 +5,7 @@ dotnet --version
 "dotnet msbuild version"
 dotnet msbuild --version
 "dotnet publish FrameworkReference"
-dotnet publish "./BugSample/WebApplication.FrameworkReference/WebApplication.FrameworkReference.csproj" --output "./SampleOutput/FrameworkReference" --verbosity=detailed /fileLogger -fileLoggerParameters:logfile="./SampleOutput/FrameworkReference/Log.log" -binaryLogger:LogFile="./SampleOutput/FrameworkReference/Log.binlog"
+dotnet publish "./BugSample/WebApplication.FrameworkReference/WebApplication.FrameworkReference.csproj" --output "./SampleOutput/FrameworkReference" --verbosity=detailed /fileLogger -fileLoggerParameters:logfile="./SampleOutput/FrameworkReference/Log.log" -binaryLogger:LogFile="./SampleOutput/FrameworkReference/Log.binlog" /p:PackageOutputPath="../../SampleOutput/FrameworkReference"
 "dotnet publish PackageReferenceDependency"
-dotnet publish "./BugSample/WebApplication.PackageReferenceDependency/WebApplication.PackageReferenceDependency.csproj" --output "./SampleOutput/PackageReferenceDependency" --verbosity=detailed /fileLogger -fileLoggerParameters:logfile="./SampleOutput/PackageReferenceDependency/Log.log" -binaryLogger:LogFile="./SampleOutput/PackageReferenceDependency/Log.binlog"
+dotnet publish "./BugSample/WebApplication.PackageReferenceDependency/WebApplication.PackageReferenceDependency.csproj" --output "./SampleOutput/PackageReferenceDependency" --verbosity=detailed /fileLogger -fileLoggerParameters:logfile="./SampleOutput/PackageReferenceDependency/Log.log" -binaryLogger:LogFile="./SampleOutput/PackageReferenceDependency/Log.binlog" /p:PackageOutputPath="../../SampleOutput/PackageReferenceDependency"
 Stop-Transcript
